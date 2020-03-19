9a60052cfba2ad79ca82a046c4c21dbaa944c36d
jahlgren <jukka.ahlgren@gmail.com>
2020-03-19 19:11:16 +0200

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const Header = ({ text }) => {
   return (
-    <h1>{text}</h1>
+    <h3>{text}</h3>
   )
 }
 
@@ -34,6 +34,7 @@ const Total = ({ parts }) => {
 }
 
 const Course = ({ course }) => {
+  console.log('in Course', course)
   return (
     <div>
       <Header text={course.name} />
@@ -45,36 +46,64 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  console.log('App toimii..')
+  console.log(courses)
+  const elements = () => courses.map(course =>
+    <Course
+      key={course.id}
+      course={course}
+    />
+  )
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curricum</h1>
+      {elements()}
     </div>
   )
 }
