74aa95634edb10e681ba495a9b12a8dcbcfdbc12
jahlgren <jukka.ahlgren@gmail.com>
2020-03-26 15:01:13 +0200

    Add proxy

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 //const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = 'https://evening-caverns-13941.herokuapp.com/api/persons'
+//const baseUrl = 'https://evening-caverns-13941.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
